eb0d3885a62fee14459e58fc890f59630f136b43
Devendra Barkhane <133559713+itsdbuniverse@users.noreply.github.com>
2024-08-07 12:26:54 +0000

    code running with api

--- app.py
+++ app.py
@@ -1,34 +1,54 @@
 from typing import Required
 from flask import Flask, render_template, jsonify
 from db_conn import query_runner
-# from api_fetch import api_fetch
+from api_fetch import api_fetch
 app = Flask(__name__)
 
-JOBS = [{
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-}, {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-}, {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-}, {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$120,000'
-}]
+# JOBS = [{
+#     'id': 1,
+#     'title': 'Data Analyst',
+#     'location': 'Bengaluru, India',
+#     'salary': 'Rs. 10,00,000'
+# }, {
+#     'id': 2,
+#     'title': 'Data Scientist',
+#     'location': 'Delhi, India',
+#     'salary': 'Rs. 15,00,000'
+# }, {
+#     'id': 3,
+#     'title': 'Frontend Engineer',
+#     'location': 'Remote'
+# }, {
+#     'id': 4,
+#     'title': 'Backend Engineer',
+#     'location': 'San Francisco, USA',
+#     'salary': '$120,000'
+# }]
 
 # html routing
+
+app = Flask(__name__)
+
 @app.route("/")
 def hello():
-  return render_template("home.html", jobs=JOBS, company_name="WIPRO")
+    responses = query_runner(query="select * from jobs")
+    data = []
+    for response in responses:
+        id, title, location, salary, currency, responsibilities, requirements = response
+        data.append({
+            'id': id,
+            'title': title,
+            'location': location,
+            'salary': salary,
+            'currency': currency,
+            'responsibilities': responsibilities,
+            'requirements': requirements
+        })
+    return render_template("home.html", jobs=data, company_name="WIPRO")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
     
 
 # json routing through api
